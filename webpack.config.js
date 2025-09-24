e01aa96c77b30d869fe189892eb0dbe1da1246c8
dianannperla <waafirmelyza@hotmail.com>
2025-09-24 15:03:29 +0100

    feat: initial commit for webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    entry: './index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /.js$/, 
+                exclude: /node_modules/, 
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env', '@babel/preset-react']
+                    }
+                }
+            }
+        ]
+    },
+    devtool: 'source-map',
+    devServer: {
+        contentBase: './dist',
+        hot: true
+    }
+};
