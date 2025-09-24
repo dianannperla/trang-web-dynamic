9a379f5ca17acad4d4aec1532b2982703fae0a63
dianannperla <waafirmelyza@hotmail.com>
2025-09-24 15:04:41 +0100

    Update webpack configuration for better compatibility with newer webpack versions.

--- webpack.config.js
+++ webpack.config.js
@@ -5,12 +5,13 @@ module.exports = {
     entry: './index.js',
     output: {
         filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist')
+        path: path.resolve(__dirname, 'dist'),
+        clean: true
     },
     module: {
         rules: [
             {
-                test: /.js$/, 
+                test: /\\.js$/, 
                 exclude: /node_modules/, 
                 use: {
                     loader: 'babel-loader',
@@ -23,7 +24,7 @@ module.exports = {
     },
     devtool: 'source-map',
     devServer: {
-        contentBase: './dist',
+        static: './dist',
         hot: true
     }
 };
