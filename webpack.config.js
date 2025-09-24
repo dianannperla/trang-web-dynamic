099b9f118e7c2b2ac7edd622e52b53e7bac00294
dianannperla <waafirmelyza@hotmail.com>
2025-09-24 15:06:51 +0100

    Update webpack configuration to enable production mode

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-    mode: 'development',
+    mode: 'production',
     entry: './index.js',
     output: {
         filename: 'bundle.js',
@@ -11,7 +11,7 @@ module.exports = {
     module: {
         rules: [
             {
-                test: /\.jsx?$/, 
+                test: \.jsx?$,
                 exclude: /node_modules/, 
                 use: {
                     loader: 'babel-loader',
